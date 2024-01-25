b2987abeab4f564b06d8f6b78fb7ca06003fec6f
mbermudez24 <mabermudez87952@umanizales.edu.co>
2024-01-24 19:16:35 -0500

    Modify the name of the images in each folder with the file t.bat

---
+++ assets/img/t .bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "prefix=image"
+
+set "counter=1"
+for %%F in (*.jpg *.png *.gif *.bmp) do (
+    set "newName=!prefix!!counter!%%~xF"g
+    ren "%%F" "!newName!"
+    set /a "counter+=1"
+)
+
+endlocal
